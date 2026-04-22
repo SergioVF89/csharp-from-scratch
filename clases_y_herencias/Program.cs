be36ad06757e9c0886bcc2b45d06e64a46fb0041
SergioVF89 <fonnegrasergio@gmail.com>
2026-04-22 14:16:05 -0500

    add cuadrado

---
+++ clases_y_herencias/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Hello, World!");
+/*clases*/
+/*var rect =new rectangulo();
+rect.Base = 20;
+rect.Altura = 40;    
+Console.WriteLine($"El area del rectangulo es: {rect.Area()}");
+Console.WriteLine($"El perimetro del rectangulo es: {rect.Perimetro()}");*/
+/*end clases*/
+
+
+var cuadrado1 = new cuadrado();
+cuadrado1.Base = 20;
+cuadrado1.Altura = 40;
+Console.WriteLine($"El area del cuadrado es: {cuadrado1.Area()}");
+Console.WriteLine($"El perimetro del cuadrado es: {cuadrado1.Perimetro()}");
+Console.WriteLine($"El lado del cuadrado es: {cuadrado1.Lado}");
+
+
+class rectangulo
+{
+   public double Base { get; set; }
+   public double Altura { get; set; }
+
+    public double Area()=> Base * Altura;
+    public double Perimetro()=> 2 * (Base + Altura);
+}
+
+
+class cuadrado: rectangulo
+{
+    public double Lado { 
+        set { Base=value; Altura = value;}
+
+        get { return Base; }    
+     
+     }
+}
+
