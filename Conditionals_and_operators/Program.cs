fe3890f1c563a0db5197d709650bef0739a7d6cb
SergioVF89 <fonnegrasergio@gmail.com>
2026-04-22 13:22:04 -0500

    add conditional

---
+++ Conditionals_and_operators/Program.cs
@@ -0,0 +1,18 @@
+﻿//string nameInput=string.Empty;
+Console.WriteLine("Ingrese un Numero: ");
+int numberInput = Convert.ToInt32(Console.ReadLine());
+
+if(numberInput > 0)
+{
+    Console.WriteLine($"El numero {numberInput} es positivo");
+}
+else if(numberInput < 0)
+{
+    Console.WriteLine($"El numero {numberInput} es negativo");
+}
+else
+{
+    Console.WriteLine($"El numero {numberInput} es cero");
+}
+
+//minuto 9 del video 11 https://www.youtube.com/watch?v=rar9qctS3wo
